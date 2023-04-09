75d3008bd2ba44768431c6599a265bef5e2bc3ec
hulkbusterks <hullkbuster@gmail.com>
2023-04-09 16:21:17 +0530

    login

---
+++ lib/Screens/Signup/components/signup_form.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/home.dart';
+
+import '../../../components/already_have_an_account_acheck.dart';
+import '../../../constants.dart';
+import '../../Login/login_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SignUpForm extends StatelessWidget {
+  const SignUpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    String emailAddress = '';
+    String password = '';
+    return Form(
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onChanged: (email) {
+              emailAddress = email!;
+            },
+            decoration: InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              textInputAction: TextInputAction.done,
+              obscureText: true,
+              cursorColor: kPrimaryColor,
+              decoration: InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+              onChanged: (pass) {
+                password = pass;
+              },
+            ),
+          ),
+          const SizedBox(height: defaultPadding / 2),
+          ElevatedButton(
+            onPressed: () async {
+              try {
+                UserCredential credential =
+                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                  email: emailAddress,
+                  password: password,
+                );
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MyHome();
+                }));
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'weak-password') {
+                  print('The password provided is too weak.');
+                } else if (e.code == 'email-already-in-use') {
+                  print('The account already exists for that email.');
+                }
+              } catch (e) {
+                print(e);
+              }
+            },
+            child: Text("Sign Up".toUpperCase()),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
