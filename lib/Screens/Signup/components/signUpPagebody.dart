6c7a34d154881befec60f3bed10e66c7caa80278
Honey bansal <honeybansal2968@gmail.com>
2023-05-04 12:09:13 +0000

    temp commit

--- lib/Screens/Signup/components/signUpPagebody.dart
+++ lib/Screens/Signup/components/signUpPagebody.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'nextpage.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:flutter_auth/Screens/utils/customField.dart';
 
 class SignUpPageBody extends StatelessWidget {
   final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
@@ -184,71 +184,6 @@ class SignUpPageBody extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class CustomTextField extends StatelessWidget {
-  double width;
-  String FieldName;
-  bool isObscure;
-  TextInputType keyboardType;
-  TextInputAction textInputAction;
-  Function(String)? onChanged;
-  String? hintText;
-  CustomTextField(
-      {Key? key,
-      required this.width,
-      required this.FieldName,
-      required this.isObscure,
-      required this.keyboardType,
-      required this.textInputAction,
-      this.onChanged,
-      this.hintText})
-      : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      width: width,
-      height: 108,
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(
-          FieldName,
-          style: GoogleFonts.getFont("Ramabhadra",fontSize: 15,fontWeight: FontWeight.bold),
-        ),
-        SizedBox(
-          height: 40,
-          child: TextFormField(
-              obscureText: isObscure,
-              textInputAction: textInputAction,
-              keyboardType: keyboardType,
-              cursorColor: Colors.black,
-              onChanged: onChanged,
-              cursorWidth: 1,
-              decoration: InputDecoration(
-                contentPadding: const EdgeInsets.only(bottom: 7, left: 10),
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(
-                    color: textFilledColor,
-                    width: 2.0,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: const BorderSide(
-                    color: Color(0xff0033ff),
-                    width: 2.0,
-                  ),
-                ),
-                fillColor: textFilledColor,
-                hintText: hintText,
-                hintStyle: TextStyle(fontSize: 14,color: Color.fromARGB(255, 195, 195, 195))
-              )),
-        )
-      ]),
     );
   }
 }
